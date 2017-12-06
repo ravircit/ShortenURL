90ce1dc398062a9e3a4a47918750b227086b9cac
Ravi <ravircit@gmail.com>
2017-12-06 18:30:16 +0530

    T12, Model updated with timestamp

--- shortener/models.py
+++ shortener/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class KirrURL(models.Model):
     url=models.CharField(max_length=200,)
     shortcode=models.CharField(max_length=15,unique=True)
+    updated = models.DateTimeField(auto_now=True)  # everytime the model is saved
+    timestamp=models.DateTimeField(auto_now_add=True)#when model was created
 
     def __str__(self):
         return str(self.url)
